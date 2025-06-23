ac07d2b9051801b210fddd3f2f4dfef6d9115531
sasha <sncoulm@gmail.com>
2025-06-23 04:29:03 +0200

    filtre

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -7,7 +7,12 @@ import { WebtoonService } from 'app/services/webtoon.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  webtoons: any[] = [];
+  allWebtoons: any[] = [];
+  filteredWebtoons: any[] = [];
+  genresDisponibles: string[] = [];
+  filtreTitre: string = '';
+  filtreGenre: string = '';
+
   page = 1;
   pageSize = 5;
 
@@ -15,17 +20,28 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.webtoonService.getAllObservable().subscribe((webtoons: any[]) => {
-      this.webtoons = webtoons;
+      this.allWebtoons = webtoons;
+      this.filteredWebtoons = webtoons;
+      this.genresDisponibles = [...new Set(webtoons.map(w => w.genre))];
+    });
+  }
+
+  filtrerWebtoons(): void {
+    this.page = 1;
+    this.filteredWebtoons = this.allWebtoons.filter(w => {
+      const correspondTitre = this.filtreTitre === '' || w.titre.toLowerCase().includes(this.filtreTitre.toLowerCase());
+      const correspondGenre = this.filtreGenre === '' || w.genre === this.filtreGenre;
+      return correspondTitre && correspondGenre;
     });
   }
 
   get paginatedWebtoons(): any[] {
     const start = (this.page - 1) * this.pageSize;
-    return this.webtoons.slice(start, start + this.pageSize);
+    return this.filteredWebtoons.slice(start, start + this.pageSize);
   }
 
   get totalPages(): number {
-    return Math.ceil(this.webtoons.length / this.pageSize);
+    return Math.ceil(this.filteredWebtoons.length / this.pageSize);
   }
 
   changerPage(page: number): void {
